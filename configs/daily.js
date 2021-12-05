4865c240b1dcd85a8bfad190d1fb1c684e8ac5be
tohrux <d1vaainiyo@gmail.com>
2021-12-06 03:03:54 +0800

    update

---
+++ configs/daily.js
@@ -0,0 +1,16 @@
+const {dailyData} = require('../data/data.js') 
+function getTodayString() {
+  const now = new Date(Date.now() + (8 * 60 * 60 * 1000))
+  const YYYY = now.getFullYear()
+  const MM = now.getMonth() + 1
+  const DD = now.getDate()
+  return `${YYYY}-${MM}-${String(DD).padStart(2, '0')}`
+}
+function getTodayDate() {
+  let todayString = getTodayString()
+  return dailyData[todayString]
+}
+
+module.exports = {
+  getTodayDate
+}
--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-const axios = require('axios');
+const axios = require('axios')
+const { getTodayDate } = require('./configs/daily.js')
+const data = require('./data/data.js')
 const url =
   'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=fb50a89c-cda0-4b85-992f-6009e99211ff'
 
-axios(url, {
+const dailyData = getTodayDate()
+const axiosConfig = {
   method: 'post',
   headers: {
     'Content-Type': 'application/json;charset=utf-8;',
   },
-  data:{
+  data: {
     msgtype: 'template_card',
     template_card: {
       card_type: 'news_notice',
       main_title: {
-        title: '🍔麦当劳今日特惠',
-        desc: '板烧鸡腿堡两件套 ¥15',
+        title: dailyData.title,
+        desc: dailyData.desc,
       },
       card_image: {
-        url: 'https://raw.githubusercontent.com/tohrux/photos/master/bot/20211205233858.png',
+        url: dailyData.img,
         aspect_ratio: 2.25,
       },
-      jump_list: [
-        {
-          type: 1,
-          url: 'https://doc.weixin.qq.com/txdoc/excel?scode=ANYAEAdoABE7c180DiAEUA5Qb0ADg&docid=e2_AEUA5Qb0ADgPHbZZfW2RTSH5LWOuw',
-          title: '📖 美食手册 📖',
-        },
-      ],
+      jump_list: data.jumpList,
       card_action: {
         type: 1,
         url: 'https://www.mcdonalds.com.cn/',
       },
     },
   },
-}).then((response) => {
-    console.log(response.data);
-})
+}
+
+axios(url, axiosConfig)
+  .then((response) => {
+    console.log(response.data)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
