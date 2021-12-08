b233687b510778c418e73c559429a1ced6355b5f
tohrux <d1vaainiyo@gmail.com>
2021-12-08 10:21:24 +0800

    add crazyThursday

--- configs/daily.js
+++ configs/daily.js
@@ -1,14 +1,16 @@
 const { dailyData } = require('../data/data.js')
-function getTodayString() {
-  const now = new Date(Date.now() + 8 * 60 * 60 * 1000)
+function getTodayString(now) {
   const YYYY = now.getFullYear()
   const MM = now.getMonth() + 1
   const DD = now.getDate()
   return `${YYYY}-${MM}-${String(DD).padStart(2, '0')}`
 }
 function getTodayDate() {
-  let todayString = getTodayString()
-  return dailyData[todayString]
+  const now = new Date(Date.now() + 8* 60 * 60 * 1000)
+  if(now.getDay()===4){
+    return dailyData['crazyThursday']
+  }
+  return dailyData[getTodayString(now)]
 }
 
 module.exports = {
